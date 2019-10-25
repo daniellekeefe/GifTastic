60d59a74c9fec2caf610089edd31405f1f5778e7
olive <daniellek@mobilefuse.com>
2019-10-25 07:15:30 -0400

    worked throuh extracting info from the button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,57 @@
 
-$("button").on("click", function() {
-    var topics = $(this).attr("data-topics");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      topics + "&api_key=Li4fJuAb969MxbQpL9hf9IvZCuZhbB1i";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
+
+  $(document).ready(function() {
+    $(document).on("click", ".search-button", function() {
+      var topics = $(this).attr("data-topics");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        topics + "&api_key=Li4fJuAb969MxbQpL9hf9IvZCuZhbB1i";
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+          var results = response.data;
+  
+          for (var i = 0; i < 10; i++) {
+            var gifDiv = $("<div>");
+  
+            var rating = results[i].rating;
+  
+            var p = $("<p>").text("Rating: " + rating);
+  
+            var topicsImage = $("<img>");
+            topicsImage.attr("src", results[i].images.fixed_height.url);
+  
+            gifDiv.prepend(p);
+            gifDiv.prepend(topicsImage);
+  
+            $("#gifs-appear-here").prepend(gifDiv);
+          }
+        });
+    });
+
+    $('#add').on('click', function() {
+      // Get value of input to create new button
+      const newButtonValue = $('#add-button').val().trim()
+      
+      // create a blank button
+      const btn = $('<button>');
+      // give the button an attribute called data-topic and give that attribute a value
+      // of input value
+      // we will use this value in our query string
+      btn.attr('data-topics', newButtonValue);
+      // give the blank button a label
+      btn.text(newButtonValue);
+      // give the blank button a class that we can add a click listener so when you click
+      // it, it will do a search based on the data-topics
+      btn.addClass('search-button');
+      // add button to screen
+      $('#buttons').prepend(btn)
+
     })
-    .then(function(response) {
-        var results = response.data;
-
-        for (var i = 0; i < 10; i++) {
-          var gifDiv = $("<div>");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var topicsImage = $("<img>");
-          topicsImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.prepend(p);
-          gifDiv.prepend(topicsImage);
-
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
-      });
-  });
+  })
 //create a gif search
 
 //Input search field (topics)
